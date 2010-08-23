19cb5b0039b096ca49d015ac75020b3ee546c41b
Ivan N. Veselov <veselov@gmail.com>
2010-08-24 01:13:48 +0300

    Added basic shell support with Shellac library.
    Shellac, mtl, shellac-haskeline added to cabal dependencies.

--- src/TdTool.hs
+++ src/TdTool.hs
@@ -9,8 +9,14 @@
 
 module Main where
 
-import System.Time
 import Data.List
+import System.Time
+
+import System.Console.Shell
+import System.Console.Shell.Backend.Haskeline
+import System.Console.Shell.ShellMonad
+
+import Control.Monad.Trans
 
 data Player = Player { pName :: String
                      , pRating :: Int
@@ -108,6 +114,11 @@ roundRobin ps = map (\(no, pairs) -> mkRoundPairings no ps pairs) . zipWith (,) 
 players :: Int -> [Player]
 players n = map (\x -> Player ("P" ++ show x) 1800 "Kyiv" "") [1 .. n]
 
+react :: String -> Sh () ()
+react s = liftIO $ putStrLn ("OK, got " ++ s)
+
+main :: IO ()
 main = do
     putStrLn "Welcome, tournament director! Let's see pairings for Round-Robin 5:"
     mapM_ (putStrLn . show) . roundRobin . players $ 5
+    runShell (mkShellDescription [] react) haskelineBackend ()
