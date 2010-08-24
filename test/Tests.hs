7c7dc999fbe9d101fbd473b1d512e72be9333113
Ivan N. Veselov <veselov@gmail.com>
2010-08-25 01:38:39 +0300

    Added test for color fair distribution: each player should play near half of the games as white in round robin.
    Also added 'containers' dependency to .cabal file since Data.Map requires it.

--- test/Tests.hs
+++ test/Tests.hs
@@ -19,6 +19,9 @@ import Test.Framework.Providers.QuickCheck (testProperty)
 import Test.QuickCheck
 import Test.HUnit
 
+import Data.List
+import qualified Data.Map as M
+
 import RoundRobin
 import qualified PairingEngine as PE
 
@@ -27,19 +30,44 @@ main = defaultMain tests
 tests = [ testGroup "Round Robin"
           [ testProperty "Number of RR rounds" propRoundsNumber
           , testProperty "Number of RR games " propGamesNumber
+          , testProperty "Number of games played as white " propWhiteGames
           ]
         ]
 -- generate n players
 players n = map (\i -> PE.Player i ('P' : show i) 1800 PE.Available) [1 .. n]
 
+pairings = makePairingsForAllRounds . players
+
 -- number of rounds got from pairings list
-rounds = length . makePairingsForAllRounds . players
+roundsNo = length . pairings
 
 -- number of games got from pairings list
-games = sum . map (\(PE.RoundPairings _ gs _) -> length gs) . makePairingsForAllRounds . players
+gamesNo = sum . map (\(PE.RoundPairings _ gs _) -> length gs) . pairings
+
+games = concatMap PE.pGames . pairings
+
+-- gives the number of games as white for each player, in the following form:
+-- Map.fromList [(P1,5),(P2,2),(P3,2),(P4,2),(P5,2),(P6,2)]
+whites = accumMapWith PE.white . games
+
+--
+-- | This function converts something like [7,2,2,2] in M.fromList [(7,1), (2,3)], showing
+-- quantity of occurenses element in given list.
+-- @f@ is a function to transform the list elements before grouping.
+--
+accumMapWith :: (Ord a, Eq a) => (b -> a) -> [b] -> M.Map a Int
+accumMapWith f = foldl' accF M.empty
+    where accF map x = M.insertWith' (+) (f x) 1 map
 
 -- rounds number is equal to 1) N if N is even 2) N - 1 if N is odd -- due to byes
-propRoundsNumber = forAll (choose (1, 10)) $ \n -> rounds n == (if even n then n - 1 else n)
+propRoundsNumber = forAll (choose (1, 10)) $ \n -> roundsNo n == (if even n then n - 1 else n)
 
 -- games number is equal to N * (N-1) / 2
-propGamesNumber = forAll (choose (1, 10)) $ \n -> games n == (n * (n - 1)) `div` 2
+propGamesNumber = forAll (choose (1, 10)) $ \n -> gamesNo n == (n * (n - 1)) `div` 2
+
+-- a number of games played as white should be fairly distributes
+-- that is, each player should play play near half of games as white
+propWhiteGames = forAll (choose (1, 10)) $ \x ->
+   let pred = if odd x then \y -> y == (x - 1) `div` 2
+                       else \y -> y == x `div` 2 || y == (x `div` 2) + 1
+   in M.null $ M.filter pred $ whites x
