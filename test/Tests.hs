c5181ae3814f16ae31fb8f272545cff2e2f6e3ec
Ivan N. Veselov <veselov@gmail.com>
2010-08-25 00:34:29 +0300

    Added QuickCheck tests for round robin games and rounds total quantity.

--- test/Tests.hs
+++ test/Tests.hs
@@ -21,20 +21,29 @@ import Test.HUnit
 
 import Data.List
 
-import RoundRobin    
-import PairingEngine    
-    
+import RoundRobin
+import qualified PairingEngine as PE
+
 main = defaultMain tests
 
 tests = [ testGroup "Round Robin"
           [ testCase "Number of rounds even" test_rr_rounds_no_1
           , testCase "Number of rounds odd" test_rr_rounds_no_2
+          , testProperty "Number of rounds in general" prop_rounds_no
+          , testProperty "Number of games in general" prop_games_no
           ]
         ]
-          
-test_rr_rounds_no_1 = length (makePairingsForAllRounds players) @?= 9
-    where players = map (\i -> Player i ("P" ++ show i) 1800 Available) [1..10]
-                    
-test_rr_rounds_no_2 = length (makePairingsForAllRounds players) @?= 3
-    where players = map (\i -> Player i ("P" ++ show i) 1800 Available) [1..3]
+-- generate n players
+players n = map (\i -> PE.Player i ("P" ++ show i) 1800 PE.Available) [1 .. n]
 
+-- number of rounds got from pairings list
+rounds = length . makePairingsForAllRounds . players
+
+-- number of games got from pairings list
+games = sum . map (\(PE.RoundPairings _ gs _) -> length gs) . makePairingsForAllRounds . players
+
+test_rr_rounds_no_1 = rounds 10 @?= 9
+test_rr_rounds_no_2 = rounds 3 @?= 3
+
+prop_rounds_no = forAll (choose (1, 10)) $ \n -> rounds n == (if even n then n - 1 else n)
+prop_games_no = forAll (choose (1, 10)) $ \n -> games n == (n * (n - 1)) `div` 2
