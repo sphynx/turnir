9e64070280944fce36861a81d6bcd0a2bdacf620
Ivan N. Veselov <veselov@gmail.com>
2010-08-26 02:54:49 +0300

    Generated pairings are now saved to the shell state.

--- src/Main.hs
+++ src/Main.hs
@@ -22,7 +22,11 @@ import Text.ParserCombinators.Parsec
 
 import Control.Monad.Trans( liftIO )
 
-data ShellState = ShellState [Player] Int
+data ShellState = ShellState {
+      stPlayers :: [Player]
+    , stMaxPlayerId :: Int
+    , stPairings :: Pairings
+    }
 
 -- shell interaction
 react :: String -> Sh ShellState ()
@@ -32,32 +36,38 @@ react s = shellPutErrLn ("Unknown command: " ++ s)
 cmds = [ exitCommand "quit"
        , helpCommand "help"
        , cmd "add" addPlayerCmd "Adds a new player"
-       , cmd "show" showPlayersCmd "Shows currently registered players"
+       , cmd "showplayers" showPlayersCmd "Shows currently registered players"
+       , cmd "showpairings" showPairingsCmd "Shows calculated pairings"
        , cmd "rr" roundRobinCmd "Makes Round-Robin pairings"
        , cmd "load" loadCmd "Loads players from file"
        ]
 
 addPlayerCmd :: String -> Sh ShellState ()
 addPlayerCmd name = do
-    modifyShellSt (\(ShellState ps maxId) -> ShellState (Player (maxId + 1) name 1800 Available : ps) (maxId + 1))
+    modifyShellSt
+      (\(ShellState ps maxId pairs) ->
+        ShellState (Player (maxId + 1) name 1800 Available : ps) (maxId + 1) pairs)
     shellPutStrLn (name ++ " added")
 
 showPlayersCmd :: Sh ShellState ()
-showPlayersCmd = do
-    (ShellState ps _) <- getShellSt
-    mapM_ (shellPutStrLn . show) ps
+showPlayersCmd = getShellSt >>= mapM_ (shellPutStrLn . show) . stPlayers
+
+showPairingsCmd :: Sh ShellState ()
+showPairingsCmd = getShellSt >>= mapM_ (shellPutStrLn . ppPairings) . stPairings
 
 roundRobinCmd :: Sh ShellState ()
 roundRobinCmd = do
-    (ShellState ps _) <- getShellSt
-    mapM_ (shellPutStrLn . ppPairings) . RR.makePairingsForAllRounds $ ps
+    st <- getShellSt
+    let pairings = RR.makePairingsForAllRounds . stPlayers $ st
+    mapM_ (shellPutStrLn . ppPairings) pairings
+    modifyShellSt (\st -> st { stPairings = pairings })
 
 loadCmd :: File -> Sh ShellState ()
 loadCmd (File f) = do
     result <- liftIO $ parseFromFile players f
     case result of
         Left err -> shellPutStrLn (show err)
-        Right ps -> putShellSt (ShellState ps 0)
+        Right ps -> putShellSt (ShellState ps 0 [])
 
 players :: Parser [Player]
 players = sepEndBy player space >>= return
@@ -86,5 +96,5 @@ shellDescr = (mkShellDescription cmds react) {
 
 main :: IO ()
 main = do
-    runShell shellDescr haskelineBackend (ShellState [] 0)
+    runShell shellDescr haskelineBackend (ShellState [] 0 [])
     return ()
